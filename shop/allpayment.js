a96eea3ee7b01ae8bace07ec03f4e3c20ff91e57
Nozyy <92721624+Nozyxid@users.noreply.github.com>
2021-11-18 17:31:39 +0700

    Add files via upload

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,21 +1,16 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
-
-*さ BANK*
-1. BRI 
-	ㅁ 5074-01-003141-52-8
-		• R***A*DI
+return `PAYMENT BY NOZZY
 		
 *さ E-MONEY*
 1. GOPAY
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ 0813 2387 0860
+		• N***Y
 2. OVO
-	ㅁ 0887 4350 47326
-		• EL*****TI
+	ㅁ 0813 2387 0860
+		• N***Y
 3. DANA
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ 0813 2387 0860
+		• N***Y
 
 *さ PULSA*
 
